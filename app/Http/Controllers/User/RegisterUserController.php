da1f595334215a1eaf11573602ea034cbb48eb36
TakumiKawaguchi0923 <takumi.ha.boc@gmail.com>
2021-07-15 09:42:49 +0900

    バリデーションを追加

--- app/Http/Controllers/User/RegisterUserController.php
+++ app/Http/Controllers/User/RegisterUserController.php
@@ -15,15 +15,29 @@ class RegisterUserController extends Controller
      * @param Request $request
      * @return JsonResponse
      */
+
     public function __invoke(Request $request): JsonResponse
     {
+        // バリデーション
+        $validated_request = $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
         $user = new User();
+        $status = 200;
 
         // fillでUserクラスのfillableの部分に追加しDBに保存
-        $user->fill($request->all())->save();
+        if (!$user->fill($validated_request)
+        ->fill($request->all())->save()) {
+            $status = 400;
+            $message = 'Bad Request';
+        }
 
         return response()->json([
-            'code' => 200,
+            'status' => $status,
         ]);
     }
 }
